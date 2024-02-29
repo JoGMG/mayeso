b3a76d5e23f20e83a559429576b88ddd96944fa1
JoGMG <joattah3@gmail.com>
2024-02-29 20:59:50 +0100

    - added port env variable

--- app/server.js
+++ app/server.js
@@ -166,4 +166,6 @@ app.delete('/exams/:id', (request, response) => {
 });
 
 // Start the server and listen on port 3000
-app.listen(port, () => console.log('Server is running on port 3000'));
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});

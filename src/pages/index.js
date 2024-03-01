b222af3fcb007dd176ec24be001ed29e11cf6709
Kwabena Boakye <kwaboakye93@gmail.com>
2024-03-01 23:42:05 +0000

    Add FAQ page to index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,10 @@
 import NotFound from "./404";
 import About from "./About";
 import CreateQuiz from "./CreateQuiz";
+import FAQ from "./FAQ";
 import Home from "./Home";
 import Quiz from "./Quiz";
 import Quizes from "./Quizes";
 import Scores from "./Scores";
-export { About, CreateQuiz, Home, NotFound, Quiz, Quizes, Scores };
+export { About, CreateQuiz, FAQ, Home, NotFound, Quiz, Quizes, Scores };
 

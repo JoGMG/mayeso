f13384644e5055ae54a9490aca638d3db133362d
Kwabena Boakye <kwaboakye93@gmail.com>
2024-03-02 00:27:59 +0000

    Add dynamic QuizID display and update Hero component

--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -1,10 +1,19 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 import { Hero } from "../components";
 
 function Quiz() {
+  const { quizId } = useParams();
   return (
     <div>
-      <Hero img="/src/assets/quizHero.gif" />
+      <Hero
+        img="/src/assets/quizHero.gif"
+        headText={"Dive into Endless Learning Possibilities"}
+        subText={"Explore Our Collection of Quizzes on Mayeso"}
+      />
+      <div className="py-10">
+        <h1 className="text-5xl text-center">QuizID: {quizId}</h1>
+      </div>
     </div>
   );
 }

aa789b0e3b678aad1cdffe1b97ab811216e790ce
Kwabena Boakye <kwaboakye93@gmail.com>
2024-03-02 00:50:13 +0000

    Update function to handle edge cases

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,101 +5,26 @@ import { Link, NavLink } from "react-router-dom";
 function Navbar() {
   const [showNav, setShowNav] = useState(false);
   return (
-    <nav className="  max-w-[1240px] mx-auto flex justify-between items-center py-1 px-4 mt-6 mb-3 lg:sticky lg:top-4">
-      <div>
-        <Link
-          to="/"
-          className="uppercase cursor-pointer text-xl font-bold hover:tracking-wider duration-200 flex gap-2 items-center"
-        >
-          <img src="/src/assets/brand.png" alt="" width={"45px"} />
-          <h1>mayeso.</h1>
-        </Link>
-      </div>
-      <div className="hidden lg:flex justify-between w-[40%] items-center ">
-        <NavLink
-          to="/about"
-          className={({ isActive }) =>
-            isActive
-              ? "uppercase cursor-pointer text-green-500"
-              : "uppercase cursor-pointer"
-          }
-        >
-          About Us
-        </NavLink>
-        <NavLink
-          to="/quiz"
-          className={({ isActive }) =>
-            isActive
-              ? "uppercase cursor-pointer text-green-500"
-              : "uppercase cursor-pointer"
-          }
-        >
-          My Quizes
-        </NavLink>
-
-        <NavLink
-          to="/scores"
-          className={({ isActive }) =>
-            isActive
-              ? "uppercase cursor-pointer text-green-500"
-              : "uppercase cursor-pointer"
-          }
-        >
-          scores
-        </NavLink>
-
-        <NavLink
-          to="/faq"
-          className={({ isActive }) =>
-            isActive
-              ? "uppercase cursor-pointer text-green-500"
-              : "uppercase cursor-pointer"
-          }
-        >
-          faq
-        </NavLink>
-      </div>
-
-      <div className="cursor-pointer">
-        <Link to={"/quiz/create"} className="hidden lg:block btn">
-          Create New Quiz
-        </Link>
-        <div
-          className="lg:hidden text-accent fixed right-4"
-          onClick={() => {
-            setShowNav((prevState) => !prevState);
-          }}
-        >
-          {showNav ? <MdOutlineClose size={30} /> : <MdOutlineMenu size={30} />}
-        </div>
-      </div>
-
-      {/* Side Nav */}
-      <div
-        className={
-          showNav
-            ? "fixed top-0 left-0 lg:hidden w-[50%] h-full  flex flex-col px-4 bg-black/50 py-7  gap-6 duration-200  shadow-sm"
-            : "fixed top-0 left-[-100%] lg:hidden w-[50%] h-full  flex flex-col px-4 bg-black/50 py-1  gap-6 duration-200  shadow-sm"
-        }
-      >
+    <nav className="bg-primary/40 lg:sticky lg:top-4 py-3">
+      <div className="  max-w-[1240px] mx-auto flex justify-between items-center py-1 px-4 mt-6 mb-3">
         <div>
-          <Link
-            to="/"
-            className="uppercase cursor-pointer text-xl font-bold hover:tracking-wider duration-200 flex gap-2 items-center"
-          >
-            <img src="/src/assets/brand.png" alt="" width={"45px"} />
-
-            <h1>mayeso.</h1>
-          </Link>
+          {!showNav && (
+            <Link
+              to="/"
+              className="uppercase cursor-pointer text-xl font-bold hover:tracking-wider duration-200 flex gap-2 items-center"
+            >
+              <img src="/src/assets/brand.png" alt="" width={"45px"} />
+              <h1>mayeso.</h1>
+            </Link>
+          )}
         </div>
-
-        <div className="flex flex-col gap-2 h-[40%] justify-between ">
+        <div className="hidden lg:flex justify-between w-[40%] items-center ">
           <NavLink
             to="/about"
             className={({ isActive }) =>
               isActive
-                ? "uppercase cursor-pointer border-b pb-3  text-center border-secondary/40 text-green-500"
-                : "uppercase cursor-pointer border-b pb-3  text-center border-secondary/40"
+                ? "uppercase cursor-pointer text-green-500"
+                : "uppercase cursor-pointer"
             }
           >
             About Us
@@ -108,39 +33,114 @@ function Navbar() {
             to="/quiz"
             className={({ isActive }) =>
               isActive
-                ? "uppercase cursor-pointer border-b pb-3  text-center border-secondary/40 text-green-500"
-                : "uppercase cursor-pointer border-b pb-3  text-center border-secondary/40"
+                ? "uppercase cursor-pointer text-green-500"
+                : "uppercase cursor-pointer"
             }
           >
             My Quizes
           </NavLink>
-
           <NavLink
             to="/scores"
             className={({ isActive }) =>
               isActive
-                ? "uppercase cursor-pointer border-b pb-3  text-center border-secondary/40 text-green-500"
-                : "uppercase cursor-pointer border-b pb-3  text-center border-secondary/40"
+                ? "uppercase cursor-pointer text-green-500"
+                : "uppercase cursor-pointer"
             }
           >
             scores
           </NavLink>
-
           <NavLink
             to="/faq"
             className={({ isActive }) =>
               isActive
-                ? "uppercase cursor-pointer border-b pb-3  text-center border-secondary/40 text-green-500"
-                : "uppercase cursor-pointer border-b pb-3  text-center border-secondary/40"
+                ? "uppercase cursor-pointer text-green-500"
+                : "uppercase cursor-pointer"
             }
           >
             faq
           </NavLink>
         </div>
-        <div className="cursor-pointer mt-4 mx-auto">
-          <Link to={"/quiz/create"} className="btn ">
+        <div className="cursor-pointer">
+          <Link to={"/quiz/create"} className="hidden lg:block btn">
             Create New Quiz
           </Link>
+          <div
+            className="lg:hidden text-accent fixed right-4"
+            onClick={() => {
+              setShowNav((prevState) => !prevState);
+            }}
+          >
+            {showNav ? (
+              <MdOutlineClose size={30} />
+            ) : (
+              <MdOutlineMenu size={30} />
+            )}
+          </div>
+        </div>
+        {/* Side Nav */}
+        <div
+          className={
+            showNav
+              ? "fixed top-0 left-0 lg:hidden w-[50%] h-full  flex flex-col px-4 bg-black/50 py-7  gap-6 duration-200  shadow-sm"
+              : "fixed top-0 left-[-100%] lg:hidden w-[50%] h-full  flex flex-col px-4 bg-black/50 py-1  gap-6 duration-200  shadow-sm"
+          }
+        >
+          <div>
+            <Link
+              to="/"
+              className="uppercase cursor-pointer text-xl font-bold hover:tracking-wider duration-200 flex gap-2 items-center"
+            >
+              <img src="/src/assets/brand.png" alt="" width={"45px"} />
+              <h1>mayeso.</h1>
+            </Link>
+          </div>
+          <div className="flex flex-col gap-2 h-[40%] justify-between ">
+            <NavLink
+              to="/about"
+              className={({ isActive }) =>
+                isActive
+                  ? "uppercase cursor-pointer border-b pb-3  text-center border-secondary/40 text-green-500"
+                  : "uppercase cursor-pointer border-b pb-3  text-center border-secondary/40"
+              }
+            >
+              About Us
+            </NavLink>
+            <NavLink
+              to="/quiz"
+              className={({ isActive }) =>
+                isActive
+                  ? "uppercase cursor-pointer border-b pb-3  text-center border-secondary/40 text-green-500"
+                  : "uppercase cursor-pointer border-b pb-3  text-center border-secondary/40"
+              }
+            >
+              My Quizes
+            </NavLink>
+            <NavLink
+              to="/scores"
+              className={({ isActive }) =>
+                isActive
+                  ? "uppercase cursor-pointer border-b pb-3  text-center border-secondary/40 text-green-500"
+                  : "uppercase cursor-pointer border-b pb-3  text-center border-secondary/40"
+              }
+            >
+              scores
+            </NavLink>
+            <NavLink
+              to="/faq"
+              className={({ isActive }) =>
+                isActive
+                  ? "uppercase cursor-pointer border-b pb-3  text-center border-secondary/40 text-green-500"
+                  : "uppercase cursor-pointer border-b pb-3  text-center border-secondary/40"
+              }
+            >
+              faq
+            </NavLink>
+          </div>
+          <div className="cursor-pointer mt-4 mx-auto">
+            <Link to={"/quiz/create"} className="btn ">
+              Create New Quiz
+            </Link>
+          </div>
         </div>
       </div>
     </nav>

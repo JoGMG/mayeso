1342bfd40f03b8b6885c545551c10ae9bff16bea
Kwabena Boakye <kwaboakye93@gmail.com>
2024-03-02 00:26:17 +0000

    Add FAQ data to faq.js

---
+++ src/assets/faq.js
@@ -0,0 +1,26 @@
+const faqData = {
+    FAQ: [
+        {
+            question: "How do I create a quiz?",
+            answer:
+                "Creating a quiz will be easy and fun! Simply sign up for a Mayeso account, and then navigate to the 'Create Quiz' page. From there, you can add questions, answer choices, and correct answers to build your quiz. Once you're done, you can share your quiz with others and start learning!",
+        },
+        {
+            question: "Can I share my quiz with others?",
+            answer:
+                "Absolutely! Once you've created a quiz, you can share it with others by generating a unique quiz link or by inviting users to take the quiz directly from your account. You can also share your quiz on social media platforms for wider reach.",
+        },
+        {
+            question: "How do I view my quiz scores?",
+            answer:
+                "To view your quiz scores, log in to your account and navigate to the 'My Quizzes' section. Here, you'll find a list of all the quizzes you've taken, along with your scores for each quiz. You can also view detailed quiz results to see which questions you answered correctly and incorrectly.",
+        },
+        {
+            question: "Is Mayeso free to use?",
+            answer:
+                "Yes, Mayeso is completely free to use! You can create quizzes, take quizzes created by others, and explore all the features of Mayeso without any cost. Enjoy learning and expanding your knowledge with Mayeso!",
+        },
+    ],
+};
+
+export default faqData;

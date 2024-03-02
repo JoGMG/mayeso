09311d3267d11d28dbcc533b17e7e55a5b8dea2a
Kwabena Boakye <kwaboakye93@gmail.com>
2024-03-02 00:38:56 +0000

    Update FAQ page layout and styles

--- src/pages/FAQ.jsx
+++ src/pages/FAQ.jsx
@@ -12,13 +12,20 @@ function FAQ() {
           "We've Got Answers! Explore our Frequently Asked Questions to find the information you need."
         }
       />
-      <div className="container mx-auto px-4 py-8">
-        <h1 className="text-3xl font-bold mb-4">Frequently Asked Questions</h1>
+      <div className="container mx-auto px-4 py-16">
+        <h1 className="text-3xl font-bold mb-4 pb-2 text-center">
+          Frequently Asked Questions (FAQ)
+        </h1>
         <div className="space-y-4">
           {faqData.FAQ.map((faqItem, index) => (
-            <div key={index} className="border p-4 rounded shadow-md">
-              <h3 className="text-xl font-semibold mb-2">{faqItem.question}</h3>
-              <p className="text-gray-700">{faqItem.answer}</p>
+            <div
+              key={index}
+              className=" py-6 bg-white/20 px-10 rounded shadow-md"
+            >
+              <h3 className="text-xl font-semibold mb-2 text-accent">
+                {faqItem.question}
+              </h3>
+              <p className="text-secondary">{faqItem.answer}</p>
             </div>
           ))}
         </div>
